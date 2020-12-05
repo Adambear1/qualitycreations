28f7c45194c4e2000cd0a45d3504f3c9f6114c2d
Adambear1 <adamcarlbirgenheier@gmail.com>
2020-12-04 16:05:12 -0800

    .

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
 require("dotenv").config();
+const mongoose = require("mongoose");
 const db = process.env.MONGODB_URI || process.env.URI;
 
 const connectDB = async () => {
